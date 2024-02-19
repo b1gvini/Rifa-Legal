113338414ff923dcc9afbb1a067c1dadce3609ea
Vinícius de França <vgf.vinicius@gmail.com>
2024-02-18 22:51:27 -0300

    Update RifaToken.sol

--- RifaToken.sol
+++ RifaToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract RifaV2 is ERC20 {
+contract RifaToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Rifa", "RIFA") {
         _mint(msg.sender, initialSupply);
     }
